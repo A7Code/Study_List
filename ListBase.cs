69527f3a116dcfc0432031f156ecb2ed2e364247
ZhongQi <zhongqi1105@gmail.com>
2016-08-09 13:43:01 +0800

    Rename Class Names

---
+++ ListBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a7_Stack
+{
+    class ListBase
+    {
+        object _stack;
+        int _count;
+
+    }
+}
---
+++ ListValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a7_Stack
+{
+    class ListValue<T>
+    {
+        public ListValue()
+        {
+
+        }
+    }
+}
